3ce867985abeeda836953a13a54a11f440f6ca8c
set <peter55605@gmail.com>
2022-03-10 13:57:51 +0800

    feat: add node module

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+let cowsay = require("cowsay");
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
+    console.log(cowsay.say({
+        text : "I'm a moooodule",
+        e : "oO",
+        T : "U "
+    }));
+} catch (error) {
+    core.setFailed(error.message);
+}
