ad2b404278296d866a8bff3b5f1f130e5de0600f
set <peter55605@gmail.com>
2022-03-12 16:40:51 +0800

    fix: git clone not use sudo

--- works/index.js
+++ works/index.js
@@ -2,7 +2,7 @@ import { getGitTag } from '../services/index.js'
 
 
 const works = {
-    updateGit: `sudo rm -rf express-server && sudo git clone git@github.com:set55/express-server.git && cd express-server && sudo git checkout ${getGitTag()}`,
+    updateGit: `sudo rm -rf express-server && git clone git@github.com:set55/express-server.git && cd express-server && sudo git checkout ${getGitTag()}`,
     build: 'sudo docker build -t express-img2 express-server'
 }
 
