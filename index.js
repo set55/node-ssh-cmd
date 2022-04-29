e6ec3db15b81404bda3e4a2e9af383a470e64f5d
set <peter55605@gmail.com>
2022-04-29 19:48:01 +0800

    log enviroment

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ try {
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+    // console.log(`The event payload: ${payload}`)
+    console.log(`all parameters: gitRepo: ${gitRepo}, repoName: ${repoName}
+    , imageName: ${imageName}, containerName: ${containerName}, containerPort: ${containerPort}, environment: ${environment}`)
 
 } catch (error) {
     core.setFailed(error.message)
