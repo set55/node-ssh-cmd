26813a3d5b18e6d5e708878d8c630c01b2f80377
set <peter55605@gmail.com>
2022-04-30 09:18:43 +0800

    fix: deploy command add --add-host=host.docker.internal:host-gateway

--- works/index.js
+++ works/index.js
@@ -12,7 +12,7 @@ class works {
         this.params = {
             updateGit: `cd ${repoName} && git fetch && git checkout ${getGitTag()}`,
             build: `sudo docker stop ${containerName} && sudo docker rm ${containerName} && sudo docker rmi -f ${imageName} && sudo docker build -t ${imageName} ${repoName}`,
-            run: `sudo docker run -d ${environment} --name=${containerName} -p ${containerPort}:80 ${imageName}`
+            run: `sudo docker run -d --add-host=host.docker.internal:host-gateway ${environment} --name=${containerName} -p ${containerPort}:80 ${imageName}`
         }
     }
 
